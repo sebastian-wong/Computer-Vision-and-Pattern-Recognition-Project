0597a8eea719b198d51abcfd0e048db7d5d8f230
Eways <divine-phalanx@hotmail.com>
2015-11-12 20:34:37 +0800

    Added tracking for referee(yellow colour)

--- tracking.py
+++ tracking.py
@@ -28,8 +28,8 @@ import cv2
 #                 cnts2.pop(j)
 #     return cnts1,cnts2
     
-cap = cv2.VideoCapture(os.getcwd() + "/stitchedVideo.mov")
-firstFrame = cv2.imread(os.getcwd()+ "/background.jpg")
+cap = cv2.VideoCapture(os.getcwd() + "/stitchedVideo.avi")
+firstFrame = cv2.imread(os.getcwd()+ "/bg.jpg")
 
 lower_blue = np.array([110,50,50])
 upper_blue = np.array([130,255,255])
@@ -37,8 +37,8 @@ lower_red = np.array([0,140,110])
 upper_red = np.array([20,200,255])
 lower_white = np.array([100,0,100])
 upper_white = np.array([200,200,200])
-lower_yellow = np.array([100,0,100])
-upper_yellow = np.array([200,200,200])
+lower_yellow = np.array([0,155,215])
+upper_yellow = np.array([100,255,255])
 cv2.namedWindow("Final", 0)
 
 #cv2.resizeWindow("Final", 1200,500)
@@ -49,43 +49,59 @@ for i in range (0,100):
     _,frame = cap.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     
-    #find blue players
-    maskBlue = cv2.inRange(hsv, lower_blue, upper_blue)
-    res = cv2.bitwise_and(frame,frame, mask=maskBlue)
+##    #find blue players
+##    maskBlue = cv2.inRange(hsv, lower_blue, upper_blue)
+##    res = cv2.bitwise_and(frame,frame, mask=maskBlue)
+##    image = cv2.cvtColor(res, cv2.COLOR_HSV2BGR)
+##    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+##    _,threshBlue = cv2.threshold(image,25,255, cv2.THRESH_BINARY)
+##    (cntsBlue, _) = cv2.findContours(threshBlue,cv2.cv.CV_RETR_TREE,cv2.cv.CV_CHAIN_APPROX_SIMPLE)
+##    
+##    # find red players
+##    maskRed = cv2.inRange(hsv, lower_red, upper_red)
+##    res = cv2.bitwise_and(frame,frame, mask=maskRed)
+##    image = cv2.cvtColor(res, cv2.COLOR_HSV2BGR)
+##    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+##    _,threshRed = cv2.threshold(image,25,255, cv2.THRESH_BINARY)
+##    (cntsRed, _) = cv2.findContours(threshRed,cv2.cv.CV_RETR_TREE,cv2.cv.CV_CHAIN_APPROX_SIMPLE)
+        
+    # find yellow players
+    maskYellow = cv2.inRange(hsv, lower_yellow, upper_yellow)
+    res = cv2.bitwise_and(frame,frame, mask=maskYellow)
     image = cv2.cvtColor(res, cv2.COLOR_HSV2BGR)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    _,threshBlue = cv2.threshold(image,25,255, cv2.THRESH_BINARY)
-    (cntsBlue, _) = cv2.findContours(threshBlue,cv2.cv.CV_RETR_TREE,cv2.cv.CV_CHAIN_APPROX_SIMPLE)
-    
-    # find red players
-    maskRed = cv2.inRange(hsv, lower_red, upper_red)
-    res = cv2.bitwise_and(frame,frame, mask=maskRed)
-    image = cv2.cvtColor(res, cv2.COLOR_HSV2BGR)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    _,threshRed = cv2.threshold(image,25,255, cv2.THRESH_BINARY)
-    (cntsRed, _) = cv2.findContours(threshRed,cv2.cv.CV_RETR_TREE,cv2.cv.CV_CHAIN_APPROX_SIMPLE)
+    _,threshYellow = cv2.threshold(image,25,255, cv2.THRESH_BINARY)
+    (cntsYellow, _) = cv2.findContours(threshYellow,cv2.cv.CV_RETR_TREE,cv2.cv.CV_CHAIN_APPROX_SIMPLE)
         
         
         
         
-        
-    for c in cntsBlue:
+##    for c in cntsBlue:
+##        M = cv2.moments(c)
+##        if (M['m00'] != 0):
+##            cx = int(M['m10']/M['m00'])
+##            cy = int(M['m01']/M['m00'])
+##            cv2.circle(frame, (cx,cy), 15,(255,0,0),2)
+##            # (x, y, w, h) = cv2.boundingRect(c)
+##            # cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+##    for c in cntsRed:
+##        M = cv2.moments(c)
+##        if (M['m00'] != 0):
+##            cx = int(M['m10']/M['m00'])
+##            cy = int(M['m01']/M['m00'])
+##            cv2.circle(frame, (cx,cy), 15,(0,0,255),2)
+##            # (x, y, w, h) = cv2.boundingRect(c)
+##            # cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+    for c in cntsYellow:
         M = cv2.moments(c)
         if (M['m00'] != 0):
             cx = int(M['m10']/M['m00'])
             cy = int(M['m01']/M['m00'])
-            cv2.circle(frame, (cx,cy), 15,(255,0,0),2)
-            # (x, y, w, h) = cv2.boundingRect(c)
-            # cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-    for c in cntsRed:
-        M = cv2.moments(c)
-        if (M['m00'] != 0):
-            cx = int(M['m10']/M['m00'])
-            cy = int(M['m01']/M['m00'])
-            cv2.circle(frame, (cx,cy), 15,(0,0,255),2)
+            cv2.circle(frame, (cx,cy), 15,(0,255,255),2)
             # (x, y, w, h) = cv2.boundingRect(c)
             # cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-        
+         
         
         
         
