48c8c6e9a5c508e5e2f0181c7399458220a2edd2
Whisperingroad <sebastian.wong@hotmail.com>
2015-11-14 04:25:04 +0800

    corrected red and blue

--- tracking_with_blur_direct_mapping.py
+++ tracking_with_blur_direct_mapping.py
@@ -25,8 +25,6 @@ def click_and_crop(event, x, y, flags, param):
         b=frame[y,x]
         print b
 
-        
-
 def getKeypoints(img):
     clone = img.copy()
     cv2.namedWindow("image",cv2.WINDOW_NORMAL)
@@ -51,22 +49,27 @@ cap = cv2.VideoCapture(os.getcwd() + "/stitchedVideo.mov")
 #cap = cv2.VideoCapture(os.getcwd() + "/their_football_videos/right_camera.mp4")
 firstFrame = cv2.imread(os.getcwd()+ "/football_field.jpg")
 background= cv2.imread(os.getcwd()+"/background.jpg")
-lower_blue = np.array([110,50,50])
-upper_blue = np.array([130,255,255])
-lower_red = np.array([0,140,110])
+# lower_blue = np.array([110,50,50])
+# upper_blue = np.array([130,255,255])
+lower_blue = np.array([100,50,50])
+upper_blue = np.array([140,255,255])
+lower_red = np.array([0,140,90])
 upper_red = np.array([20,200,255])
-lower_white = np.array([100,0,100])
-upper_white = np.array([200,200,200])
+# lower_red = np.array([0,140,110])
+# upper_red = np.array([20,200,255])
+lower_white = np.array([100,90,100])
+upper_white = np.array([150,120,140])
 lower_yellow = np.array([30,150,200])
 upper_yellow = np.array([100,255,255])
-
-lower_green = np.array([90,150,110])
-upper_green = np.array([95,160,125])
-
-
+lower_green = np.array([50,100,100])
+upper_green = np.array([105,160,200])
+# lower_ball = np.array([30,56,175])
+# upper_ball = np.array([50,71,183])
+lower_ball = np.array([27,56,175])
+upper_ball = np.array([50,71,183])
+# upper_ball = np.array([31,94,102])
 # lower_green = np.array([50,100,100])
 # upper_green = np.array([160,210,200])
-
 frameCounts = int(cap.get(7))
 gradient=(482-116)/(4887-3126)*1.0
 intecept=116-(3126*gradient)
@@ -87,14 +90,12 @@ BottomLeftPointAfter  = (float(89), float(535))
 fieldCoordsB = [TopLeftPointBefore, TopRightPointBefore, BottomRightPointBefore, BottomLeftPointBefore]
 fieldCoordsA = [TopLeftPointAfter , TopRightPointAfter, BottomRightPointAfter, BottomLeftPointAfter]
 
-#getKeypoints(firstFrame)
-
 frameCounts = int(cap.get(7))
 print frameCounts
 BEHomographyMatrix, status = cv2.findHomography(np.array(fieldCoordsA), np.array(fieldCoordsB), 0)
 BEHomographyMatrix=np.array(BEHomographyMatrix,dtype='float32')
 fourcc = cv2.cv.CV_FOURCC('D', 'I', 'V','X')
-video = cv2.VideoWriter("bird_eye_view_mappedVideo.avi",fourcc,24,(1200,800),True)
+video = cv2.VideoWriter("bird_eye_view_mappedVideo.avi",fourcc,24,(600,376),True)
 
 for i in range (0,1000):
 
@@ -102,51 +103,61 @@ for i in range (0,1000):
     bluepoints=[]
     yellowpoints=[]        
     greenpoints=[]
+    whitepoints=[]
+    ballpoints = []
+    
     mappedbluepoints=[]
     mappedredpoints=[]
     mappedyellowpoints=[]
     mappedgreenpoints=[]
     _,frame = cap.read()
-    #getKeypoints(frame)
+
     diff=cv2.absdiff(frame,background)
     diff_hsv=cv2.cvtColor(diff,cv2.COLOR_BGR2HSV)
     _,_,value=cv2.split(diff_hsv)
     _,thresAbsDiff=cv2.threshold(value,40,255, cv2.THRESH_BINARY)
     res=cv2.bitwise_and(frame,frame,mask=thresAbsDiff)
     hsv = cv2.cvtColor(res, cv2.COLOR_BGR2HSV)
-
-    #resize = cv2.resize(thresAbsDiff,(1200,800))
-
-    #cv2.imshow("threshold",resize)
     kernel = np.ones((5,5),np.uint8)
     firstFramecopy=firstFrame.copy()
-
+    
+    # find ball
+    maskBall = cv2.inRange(hsv, lower_ball, upper_ball)
+    res = cv2.bitwise_and(frame,frame, mask=maskBall)
+    # cv2.imshow('res', res)
+    # cv2.waitKey(0)
+    h,s,v = cv2.split(res)
+    # cv2.imshow('h',h)
+    # cv2.waitKey(0)
+    blurred = cv2.GaussianBlur(h, (33,33),1)
+    dilation = cv2.dilate(blurred,kernel,iterations = 6)
+    _,threshBall = cv2.threshold(dilation,25,255, cv2.THRESH_BINARY)
+    (cntsBall, _) = cv2.findContours(threshBall,cv2.cv.CV_RETR_TREE,cv2.cv.CV_CHAIN_APPROX_SIMPLE)
+    
     # find blue players
     maskBlue = cv2.inRange(hsv, lower_blue, upper_blue)
-    res = cv2.bitwise_and(frame,frame, mask=maskBlue)
+    res = cv2.bitwise_and(hsv,hsv, mask=maskBlue)    
     image = cv2.cvtColor(res, cv2.COLOR_HSV2BGR)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    blurred = cv2.GaussianBlur(image, (33,33),1)
-    # cv2.imshow('blur',blurred)
-    # cv2.waitKey(0)
-    dilation = cv2.dilate(blurred,kernel,iterations = 6)
-    # cv2.imshow('dilation',dilation)
-    # cv2.waitKey(0)
-    _,threshBlue = cv2.threshold(dilation,10,255, cv2.THRESH_BINARY)
+    # res = cv2.bitwise_and(frame,frame, mask=maskBlue)
+    # image = cv2.cvtColor(res, cv2.COLOR_HSV2BGR)
+    # image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # blurred = cv2.GaussianBlur(image, (33,33),1)
+    dilation = cv2.dilate(image,kernel,iterations = 5)
+    _,threshBlue = cv2.threshold(dilation,25,255, cv2.THRESH_BINARY)
     (cntsBlue, _) = cv2.findContours(threshBlue,cv2.cv.CV_RETR_TREE,cv2.cv.CV_CHAIN_APPROX_SIMPLE)
 
     # find red players
     maskRed = cv2.inRange(hsv, lower_red, upper_red)
-    res = cv2.bitwise_and(frame,frame, mask=maskRed)
+    res = cv2.bitwise_and(hsv,hsv, mask=maskRed)
     image = cv2.cvtColor(res, cv2.COLOR_HSV2BGR)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # res = cv2.bitwise_and(frame,frame, mask=maskRed)
+    # image = cv2.cvtColor(res, cv2.COLOR_HSV2BGR)
+    # image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     blurred = cv2.GaussianBlur(image, (33,33),1)
-    # cv2.imshow('blur',blurred)
-    # cv2.waitKey(0)
-    dilation = cv2.dilate(blurred,kernel,iterations = 6)
-    # cv2.imshow('dilation',dilation)
-    # cv2.waitKey(0)
-    _,threshRed = cv2.threshold(dilation,10,255, cv2.THRESH_BINARY)
+    dilation = cv2.dilate(blurred,kernel,iterations = 5)
+    _,threshRed = cv2.threshold(dilation,25,255, cv2.THRESH_BINARY)
     (cntsRed, _) = cv2.findContours(threshRed,cv2.cv.CV_RETR_TREE,cv2.cv.CV_CHAIN_APPROX_SIMPLE)
 
     # find yellow players
@@ -169,41 +180,63 @@ for i in range (0,1000):
     _,threshGreen = cv2.threshold(dilation,10,255, cv2.THRESH_BINARY)
     (cntsGreen, _) = cv2.findContours(threshGreen,cv2.cv.CV_RETR_TREE,cv2.cv.CV_CHAIN_APPROX_SIMPLE)
     
+    # find white players
+    diff = cv2.absdiff(frame,background)    
+    maskWhite = cv2.inRange(diff, lower_white, upper_white)
+    res = cv2.bitwise_and(diff,diff, mask=maskWhite)
+    image = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(image, (33,33),3)
+    dilation = cv2.dilate(blurred,kernel,iterations = 6)
+    _,threshWhite = cv2.threshold(dilation,6,255, cv2.THRESH_BINARY)
+    (cntsWhite, _) = cv2.findContours(threshWhite,cv2.cv.CV_RETR_TREE,cv2.cv.CV_CHAIN_APPROX_SIMPLE)
+    
+    
+    # for c in cntsBall:
+    #     M = cv2.moments(c)
+    #     result = cv2.approxPolyDP(c,0.1,True)
+    #     rows,columns,pointValues = result.shape
+    #     # rows,columns = result.shape
+    #     # if cv2.contourArea(c) > 675 and (rows<4 and rows>6):
+    #     if cv2.contourArea(c) > 675 or (rows<4 and rows>6):
+    #         # move to next contour
+    #         continue
+    #     if (M['m00'] != 0):
+    #         cx = int(M['m10']/M['m00'])
+    #         cy = int(M['m01']/M['m00'])
+    #         if cy >100 :
+    #             # cv2.circle(frame, (cx,cy), 15,(0,0,0),1)
+    #             ballpoints.append([cx,cy])
+    #             ballpt = sorted(ballpoints)[-1]
+    # cv2.circle(frame, (ballpt[0],ballpt[1]), 15,(0,0,0),1)
+    # print "ball " + str(i)
+    # print cx,cy
+    # print rows
+
                      
     for c in cntsBlue:
-        # if cv2.contourArea(c) < 100:
-        #     # move to next contour
-        #     continue
         M = cv2.moments(c)
         if (M['m00'] != 0):
             cx = int(M['m10']/M['m00'])
             cy = int(M['m01']/M['m00'])
             if cy >100 :
-                #cv2.circle(frame, (cx,cy), 15,(255,0,0),2)
+                cv2.circle(frame, (cx,cy), 15,(255,0,0),2)
                 bluepoints.append([cx,cy])
             
     for c in cntsRed:
-        # if cv2.contourArea(c) < 5:
-        #     # move to next contour
-        #     continue
         M = cv2.moments(c)
         if (M['m00'] != 0):
             cx = int(M['m10']/M['m00'])
             cy = int(M['m01']/M['m00'])
             if cy >100:
-                #cv2.circle(frame, (cx,cy), 15,(0,0,255),2)
+                cv2.circle(frame, (cx,cy), 15,(0,0,255),2)
                 redpoints.append([cx,cy])
 
     for c in cntsYellow:
-        # if cv2.contourArea(c) < 200:
-        #     # move to next contour
-        #     continue
         M = cv2.moments(c)
         if (M['m00'] != 0):
             cx = int(M['m10']/M['m00'])
             cy = int(M['m01']/M['m00'])
             if cy >100:
-                #cv2.circle(frame, (cx,cy), 15,(0,255,255),2)
                 yellowpoints.append([cx,cy])
             
     for c in cntsGreen:
@@ -215,10 +248,17 @@ for i in range (0,1000):
             cx = int(M['m10']/M['m00'])
             cy = int(M['m01']/M['m00'])
             if cy >100:
-                cv2.circle(frame, (cx,cy), 15,(0,255,0),2)
                 greenpoints.append([cx,cy])
-                
-                
+                   
+    for c in cntsWhite:
+        M = cv2.moments(c)
+        if cv2.contourArea(c) >1000:
+            if (M['m00'] != 0):
+                cx = int(M['m10']/M['m00'])
+                cy = int(M['m01']/M['m00'])
+                if (175<cx<2542 and 116<cy<519):
+                    whitepoints.append([cx,cy])
+                                          
     redPlayers = []
     bluePlayers = []            
 
@@ -234,7 +274,6 @@ for i in range (0,1000):
     for pts in redpoints:
         pt=np.array([[[pts[0],pts[1]]]],dtype='float32')
         pt=cv2.perspectiveTransform(pt,BEHomographyMatrix)
-        #mappedredpoints.append(pt)
         if (pt[0][0][0]>20 and pt[0][0][0]<579 and 10<pt[0][0][1]<365):
             redPlayers.append([pt[0][0][0],pt[0][0][1]])
             redPlayers = sorted(redPlayers)    
@@ -243,22 +282,29 @@ for i in range (0,1000):
     for pts in yellowpoints:
         pt=np.array([[[pts[0],pts[1]]]],dtype='float32')
         pt=cv2.perspectiveTransform(pt,BEHomographyMatrix)
-        #mappedyellowpoints.append(pt)
         if (pt[0][0][0]>20 and pt[0][0][0]<579):
             cv2.circle(firstFramecopy, (pt[0][0][0],pt[0][0][1]), 5,(0,255,255),-1)
 
     for pts in greenpoints:
         pt=np.array([[[pts[0],pts[1]]]],dtype='float32')
         pt=cv2.perspectiveTransform(pt,BEHomographyMatrix)
-        #mappedgreenpoints.append(pt)
         if (pt[0][0][0]>20 and pt[0][0][0]<579 and 10<pt[0][0][1]<365):
             cv2.circle(firstFramecopy, (pt[0][0][0],pt[0][0][1]), 5,(0,255,0),-1)
+            
+    for pts in whitepoints:
+        pt=np.array([[[pts[0],pts[1]]]],dtype='float32')
+        pt=cv2.perspectiveTransform(pt,BEHomographyMatrix)
+        if (pt[0][0][0]>20 and pt[0][0][0]<579 and 10<pt[0][0][1]<365):
+            cv2.circle(firstFramecopy, (pt[0][0][0],pt[0][0][1]), 5,(255,255,255),-1)        
 
-    
+
+    # for offsight
+    # left goalpost belongs to red players
     redOffSightX = redPlayers[0][0]
     redOffSight = ((redOffSightX,0),(redOffSightX,4902))
     cv2.line(firstFramecopy,redOffSight[0],redOffSight[1],(0,0,255),thickness = 1)
     
+    # right goalpost belongs to blue players
     blueOffSightX = bluePlayers[-1][0]
     blueOffSight = ((blueOffSightX,0),(blueOffSightX,4902))
     cv2.line(firstFramecopy,blueOffSight[0],blueOffSight[1],(255,0,0),thickness = 1)
@@ -267,19 +313,17 @@ for i in range (0,1000):
     resize = cv2.resize(firstFramecopy,(600,376))
     if i%10 == 0:
         print i
-        #cv2.imwrite("AfterWarpImage"+str(i)+".jpg", firstFramecopy)
-    #video.write(resize)
-    # resize = cv2.resize(frame,(1200,800))
+
     cv2.imshow("Final", resize)
-    cv2.waitKey(30)
+    # cv2.imshow("ballie",frame)
+    
+    cv2.waitKey(0)
     key = cv2.waitKey(1) & 0xFF
     if key == ord("c"):
         break
-cv2.imwrite("AfterWarpImage.jpg", firstFrame)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 video.release()
-
 cap.release()
 
